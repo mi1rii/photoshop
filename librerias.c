b8cae9e20af58e8cfd5902ab52d0b26d7e34227e
Fanilex <a01736897@tec.mx>
2026-04-13 11:59:17 -0600

    Paralelizar librerias con OpenMP

--- librerias.c
+++ librerias.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <omp.h>
 #include "selec_proc.h"
 #include "selec_proc_1.h"
+
+#define NUM_THREADS 6
 
 int main(){
     FILE *archivo;
     char data[80] = "arc1.txt";
+
+    omp_set_num_threads(NUM_THREADS);
+
     archivo = fopen(data, "w");
     if (archivo == NULL){
         printf("Error al abrir el archivo");
@@ -16,11 +22,26 @@ int main(){
     fprintf(archivo, "Estefania Antonio\n");
     fclose(archivo);
 
-    inv_img("inv_1","prueba1.bmp");
-    inv_img_lado("inv_1_lado","prueba1.bmp");
-    inv_img_color("inv_1_color","prueba1.bmp");
-    inv_img_color_lado("inv_1_color_lado","prueba1.bmp");
-    desenfoque_bn("prueba1.bmp","desenfoque_bn.bmp", 47);
-    desenfoque_color("prueba1.bmp","desenfoque_color.bmp", 47);
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        inv_img("inv_1","prueba1.bmp");
+
+        #pragma omp section
+        inv_img_lado("inv_1_lado","prueba1.bmp");
+
+        #pragma omp section
+        inv_img_color("inv_1_color","prueba1.bmp");
+
+        #pragma omp section
+        inv_img_color_lado("inv_1_color_lado","prueba1.bmp");
+
+        #pragma omp section
+        desenfoque_bn("prueba1.bmp","desenfoque_bn.bmp", 47);
+
+        #pragma omp section
+        desenfoque_color("prueba1.bmp","desenfoque_color.bmp", 47);
+    }
+
     return 0;
 }
