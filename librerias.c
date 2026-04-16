216a01b55603549ae899b1be42f8fef0c809651c
mii1rii <mirandaarronizz@gmail.com>
2026-04-16 12:04:36 -0600

    feat: add support for multiple threads

--- librerias.c
+++ librerias.c
@@ -7,8 +7,6 @@
 #define NUM_THREADS 6
 
 int main() {
-    clock_t start_time = clock();
-    
     FILE *fptr;
     char data[80] = "results/arc1.txt";
     system("mkdir -p results");
@@ -29,68 +27,84 @@ int main() {
     };
     int num_images = 4;
 
-    // Procesar cada imagen
-    for (int img_idx = 0; img_idx < num_images; img_idx++) {
-        // Extraer nombre base de la imagen (sin ruta ni extensión)
-        char img_name[50];
-        const char *filename = strrchr(images[img_idx], '/') + 1;  // Obtener nombre después del /
-        strcpy(img_name, filename);
-        char *dot = strrchr(img_name, '.');
-        if (dot) *dot = '\0';  // Remover extensión .bmp
+    // Array de configuraciones de threads
+    int thread_configs[] = {6, 12, 18};
+    int num_configs = 3;
+
+    // Ejecutar 3 veces con diferente número de threads
+    for (int config_idx = 0; config_idx < num_configs; config_idx++) {
+        int num_threads = thread_configs[config_idx];
+        printf("%d threads:\n", num_threads);
         
-        #pragma omp parallel
-        {
-            #pragma omp sections
+        clock_t start_time = clock();
+        
+        // Procesar cada imagen
+        for (int img_idx = 0; img_idx < num_images; img_idx++) {
+            // Extraer nombre base de la imagen (sin ruta ni extensión)
+            char img_name[50];
+            const char *filename = strrchr(images[img_idx], '/') + 1;
+            strcpy(img_name, filename);
+            char *dot = strrchr(img_name, '.');
+            if (dot) *dot = '\0';
+            
+            // Agregar sufijo de threads al nombre
+            char img_name_with_threads[80];
+            sprintf(img_name_with_threads, "%s_t%d", img_name, num_threads);
+            
+            #pragma omp parallel num_threads(num_threads)
             {
-                #pragma omp section
+                #pragma omp sections
                 {
-                    char output[100];
-                    sprintf(output, "results/%s_inv", img_name);
-                    inv_img(output, images[img_idx]);
-                }
-                #pragma omp section
-                {
-                    char output[100];
-                    sprintf(output, "results/%s_espejo_gris", img_name);
-                    inv_img_grey_horizontal(output, images[img_idx]);
-                }
-                #pragma omp section
-                {
-                    char output[100];
-                    sprintf(output, "results/%s_inv_color", img_name);
-                    inv_img_color(output, images[img_idx]);
-                }
-                #pragma omp section
-                {
-                    char output[100];
-                    sprintf(output, "results/%s_inv_bn_vertical", img_name);
-                    inv_img_bn_vertical(output, images[img_idx]);
-                }
-                #pragma omp section
-                {
-                    char output[100];
-                    sprintf(output, "results/%s_desenfoque", img_name);
-                    desenfoque(images[img_idx], output, 27);
-                }
-                #pragma omp section
-                {
-                    char output[100];
-                    sprintf(output, "results/%s_desenfoque_bn", img_name);
-                    desenfoque_gris(images[img_idx], output, 27);
-                }
-                #pragma omp section
-                {
-                    char output[100];
-                    sprintf(output, "results/%s_espejo_color", img_name);
-                    inv_img_color_horizontal(output, images[img_idx]);
+                    #pragma omp section
+                    {
+                        char output[100];
+                        sprintf(output, "results/%s_inv", img_name_with_threads);
+                        inv_img(output, images[img_idx]);
+                    }
+                    #pragma omp section
+                    {
+                        char output[100];
+                        sprintf(output, "results/%s_espejo_gris", img_name_with_threads);
+                        inv_img_grey_horizontal(output, images[img_idx]);
+                    }
+                    #pragma omp section
+                    {
+                        char output[100];
+                        sprintf(output, "results/%s_inv_color", img_name_with_threads);
+                        inv_img_color(output, images[img_idx]);
+                    }
+                    #pragma omp section
+                    {
+                        char output[100];
+                        sprintf(output, "results/%s_inv_bn_vertical", img_name_with_threads);
+                        inv_img_bn_vertical(output, images[img_idx]);
+                    }
+                    #pragma omp section
+                    {
+                        char output[100];
+                        sprintf(output, "results/%s_desenfoque", img_name_with_threads);
+                        desenfoque(images[img_idx], output, 27);
+                    }
+                    #pragma omp section
+                    {
+                        char output[100];
+                        sprintf(output, "results/%s_desenfoque_bn", img_name_with_threads);
+                        desenfoque_gris(images[img_idx], output, 27);
+                    }
+                    #pragma omp section
+                    {
+                        char output[100];
+                        sprintf(output, "results/%s_espejo_color", img_name_with_threads);
+                        inv_img_color_horizontal(output, images[img_idx]);
+                    }
                 }
             }
         }
+        
+        clock_t end_time = clock();
+        double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+        printf("%.2f segundos\n", elapsed_time);
     }
-    
-    clock_t end_time = clock();
-    double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-    printf("Tiempo de ejecucion: %.2f segundos\n", elapsed_time);
     
     return 0;
 }
